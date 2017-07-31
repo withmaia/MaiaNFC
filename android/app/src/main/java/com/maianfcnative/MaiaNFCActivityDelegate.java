6450f0b2e6e853e6c861f680163f973cb00e7e9f
Sean Robertson <sprobertson@gmail.com>
2017-07-31 11:25:23 -0700

    emit event when nfc intent happens while active

--- android/app/src/main/java/com/maianfcnative/MaiaNFCActivityDelegate.java
+++ android/app/src/main/java/com/maianfcnative/MaiaNFCActivityDelegate.java
@@ -1,7 +1,6 @@
 package com.maianfcnative;
 
 import android.os.Bundle;
-import android.content.Context;
 import android.content.Intent;
 import android.app.Activity;
 
@@ -14,9 +13,12 @@ import android.util.Log;
 import javax.annotation.Nullable;
 
 import com.facebook.react.ReactActivityDelegate;
+import com.facebook.react.modules.core.DeviceEventManagerModule;
+import com.facebook.react.bridge.Arguments;
+import com.facebook.react.bridge.WritableMap;
 
 public class MaiaNFCActivityDelegate extends ReactActivityDelegate {
-    private Bundle mInitialProps = null;
+    private Bundle initialProps = null;
     private final @Nullable Activity activity;
 
     public MaiaNFCActivityDelegate(Activity activity, String mainComponentName) {
@@ -26,35 +28,56 @@ public class MaiaNFCActivityDelegate extends ReactActivityDelegate {
     }
 
     @Override
+    protected Bundle getLaunchOptions() {
+        return initialProps;
+    }
+
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
-        Log.i("ACTIVITY DELEGATE", "onCreate");
-        mInitialProps = new Bundle();
+        initialProps = new Bundle();
         Intent intent = activity.getIntent();
         String action = intent.getAction();
 
         if (action != null && action.equals(NfcAdapter.ACTION_NDEF_DISCOVERED)) {
-            Log.i("ACTIVITY DELEGATE", action);
-            Parcelable[] rawMsgs = intent.getParcelableArrayExtra(NfcAdapter.EXTRA_NDEF_MESSAGES);
-
-            for (int i = 0; i < rawMsgs.length; i++) {
-                NdefMessage msg = (NdefMessage) rawMsgs[i];
-                NdefRecord record = msg.getRecords()[0];
-                byte[] payload = record.getPayload();
-                String payload_string = new String(payload);
-
-                mInitialProps.putString("payload", payload_string);
-                mInitialProps.putString("mime_type", record.toMimeType());
-            }
+            WritableMap map = parseNFCIntent(intent);
+            initialProps.putString("mime_type", map.getString("payload"));
+            initialProps.putString("mime_type", map.getString("mime_type"));
         }
 
-        Log.i("ACTIVITY DELEGATE", "end of onCreate");
         super.onCreate(savedInstanceState);
     }
 
     @Override
-    protected Bundle getLaunchOptions() {
-        return mInitialProps;
+    public boolean onNewIntent(Intent intent) {
+        String action = intent.getAction();
+        if (action != null && action.equals(NfcAdapter.ACTION_NDEF_DISCOVERED)) {
+            Log.i("NEW INTENT", action);
+            WritableMap map = parseNFCIntent(intent);
+            getReactInstanceManager().getCurrentReactContext()
+                .getJSModule(DeviceEventManagerModule.RCTDeviceEventEmitter.class)
+                .emit("new_tag", map);
+
+        }
+        return super.onNewIntent(intent);
     }
+
+    private WritableMap parseNFCIntent(Intent intent) {
+        Parcelable[] rawMsgs = intent.getParcelableArrayExtra(NfcAdapter.EXTRA_NDEF_MESSAGES);
+        WritableMap map = Arguments.createMap();
+
+        for (int i = 0; i < rawMsgs.length; i++) {
+            NdefMessage msg = (NdefMessage) rawMsgs[i];
+            NdefRecord record = msg.getRecords()[0];
+            byte[] payload = record.getPayload();
+            String payload_string = new String(payload);
+
+            map.putString("payload", payload_string);
+            map.putString("mime_type", record.toMimeType());
+        }
+
+        return map;
+    }
+
 };
 
 
